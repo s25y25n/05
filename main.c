449f9f76de0006098f42fc0a324303c2879eed4f
s25y25n <144671761+s25y25n@users.noreply.github.com>
2023-10-05 17:15:10 +0900

    06
    실습6

--- main.c
+++ main.c
@@ -3,22 +3,27 @@
 
 int main(int argc, char *argv[])
 {
-  int num ;
-  int sum = 0 ;
-  int i ;
-  
-  printf("input a number : ");
-  scanf("%d" , & num);
-  
-  for ( i = 0; i <= num ; i++ ) {
-      sum = sum + i ;
-      }
-  
-  printf("Result is %d \n", sum);
-  
-  
-  
-  
+    int answer = 32 ;
+    int num ;
+    int trial = 0;
+    
+    do
+    {
+        //1. input
+        printf("Guess a number :");
+        scanf("%d", &num);
+        
+        //2. decision low high
+        if(num < answer)
+          printf("low!\n");
+        else if(num > answer)
+          printf("high!\n");
+        
+        trial += 1;
+    } while(num != answer);
+    
+    printf("Congratulation! trial : %d \n", trial);
+
   system("PAUSE");	
   return 0;
 }
