19dd8c8767fd62ff9685dc98be8c68b1b5aaaf28
s25y25n <144671761+s25y25n@users.noreply.github.com>
2023-10-05 15:51:19 +0900

    02
    실습2

--- main.c
+++ main.c
@@ -5,18 +5,15 @@ int main(int argc, char *argv[])
 {
     int a ;
     
-    printf("정수 하나를 입력하세요 : ");
-    scanf("%i" , &a);
+    printf("정수를 입력하시오 : ");
+    scanf("%d", &a);
     
-    if(a < 0) {
-         printf("음수입니다\n");
-         } 
-    else if (a > 0) {
-         printf("양수입니다\n");
-         }
-    else {
-         printf("0\n");
-         } 
+    if(a<0) {
+         a = a * -1;
+    }
+         
+    printf("절대값은 %d 입니다.", a);
+    
   system("PAUSE");	
   return 0;
 }
